fceab93e7fe1da109ea3908ad307ee3bdcea4918
Adelaide Cristina Sá <90859970+adecris@users.noreply.github.com>
2025-04-06 23:20:16 -0300

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,36 @@
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('papelaria-cache-v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/manifest.json',
+        '/icon-192.png',
+        '/icon-512.png'
+        // Adicione aqui outros arquivos importantes do app
+      ]);
+    })
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(
+        keys.filter(key => key !== 'papelaria-cache-v1')
+            .map(key => caches.delete(key))
+      )
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
